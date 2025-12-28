ddf43346d7184eda041a69da04af1c1ecb6bc06b
kamshetty varun <varunn912>
2025-12-28 18:00:10 +0530

    Convert ResearchAgent to Class structure and include Tavily tools

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -1,15 +1,15 @@
-from .research_agent import create_research_agent
-from .summarizer_agent import create_summarizer_agent
-from .fact_checker_agent import create_fact_checker_agent
-from .trend_analyst_agent import create_trend_analyst_agent
-from .report_writer_agent import create_report_writer_agent
-from .content_extractor_agent import create_content_extractor_agent
+from .research_agent import ResearchAgent
+from .summarizer_agent import SummarizerAgent
+from .fact_checker_agent import FactCheckerAgent
+from .trend_analyst_agent import TrendAnalystAgent
+from .report_writer_agent import ReportWriterAgent
+from .content_extractor_agent import ContentExtractorAgent
 
 __all__ = [
-    'create_research_agent',
-    'create_summarizer_agent',
-    'create_fact_checker_agent',
-    'create_trend_analyst_agent',
-    'create_report_writer_agent',
-    'create_content_extractor_agent'
+    'ResearchAgent',
+    'SummarizerAgent',
+    'FactCheckerAgent',
+    'TrendAnalystAgent',
+    'ReportWriterAgent',
+    'ContentExtractorAgent'
 ]
